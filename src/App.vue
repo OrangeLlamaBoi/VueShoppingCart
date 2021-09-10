58deab3ad20b212dd207b05205d1c8b31b599b92
Damien Lamont <damienlama@gmail.com>
2021-09-10 10:48:56 +0200

    storeJS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <nav>
+      <h2>CupCakes</h2>
+    </nav>
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <div class="bg-red-500">hi</div>
   </div>
@@ -17,14 +19,3 @@ export default {
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
